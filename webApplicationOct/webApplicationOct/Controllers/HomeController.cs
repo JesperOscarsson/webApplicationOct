224fd104bbccf0ec76d5eb49ac189055246f0129
Admin <jejo7777@gmail.com>
2016-10-08 09:37:02 +0200

    Added "About" view

--- webApplicationOct/webApplicationOct/Controllers/HomeController.cs
+++ webApplicationOct/webApplicationOct/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace webApplicationOct.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
     }
 }
