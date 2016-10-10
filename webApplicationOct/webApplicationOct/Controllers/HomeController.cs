37d7f7e314e0cc513d8aa18d2604dab30d802d54
Admin <jejo7777@gmail.com>
2016-10-10 19:58:55 +0200

    Added properties to Education class. Added list in Controllers and gave it some "Education" type objects

--- webApplicationOct/webApplicationOct/Controllers/HomeController.cs
+++ webApplicationOct/webApplicationOct/Controllers/HomeController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using webApplicationOct.Models;
 
 namespace webApplicationOct.Controllers
 {
     public class HomeController : Controller
-    {     
+    {
+        private List<object> ExperienceList;
+
+        public HomeController()
+        {
+            ExperienceList = new List<object>();
+            //Below add stuff to list
+            ExperienceList.Add(new Education() { Course = "Placeholder1", Period = "1786 - 2016", School = "Östra" });
+            ExperienceList.Add(new Education() { Course = "Placeholder2", Period = "1786 - 2016", School = "Östra" });
+            ExperienceList.Add(new Education() { Course = "Placeholder3", Period = "1786 - 2016", School = "Östra" });
+        }       
+        
         // GET: Home
         public ActionResult Index()
         {
--- webApplicationOct/webApplicationOct/Models/Experience.cs
+++ webApplicationOct/webApplicationOct/Models/Experience.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace webApplicationOct
+namespace webApplicationOct.Models
 {
     public class Education
     {
         public Education()
         {
+        }
+
+        public string Period { get; set; }
+        public string School { get; set; }
+        public string Course { get; set; }
+
+        public string getEducation()
+        {
+            return string.Format("Studied {0} at {1} during {2}", Course, School, Period);
         }
     }
 
